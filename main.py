22171b5093e69d8f7c69b89fb8ec7d0bc2cb9736
miguee20 <124699281+miguee20@users.noreply.github.com>
2023-02-14 09:45:52 -0600

    Cambios
    Se agrego un .gitignore el cual omite la carpeta de modulos

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+print('Miguel Antonio Salguero Sandoval')
+mi_ubicacion = os.getcwd()
+if os.path.exists("modulos"):
+    print("La carpeta ya existe")
+else:
+    os.mkdir(mi_ubicacion + "\\modulos")
+    archivo = open('./modulos/prueba.txt', 'w')
+    archivo.write('Hola mundo')
+    archivo.close()
